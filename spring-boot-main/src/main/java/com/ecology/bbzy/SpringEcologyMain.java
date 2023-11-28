e8d0fa7364207c7c49f5e534608323d721644cae
bbzyalone <bbzykkall@outlook.com>
2023-11-29 00:26:31 +0800

    ss

---
+++ spring-boot-main/src/main/java/com/ecology/bbzy/SpringEcologyMain.java
@@ -0,0 +1,15 @@
+package com.ecology.bbzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * SpringBootApplication
+ */
+@SpringBootApplication
+public class SpringEcologyMain {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEcologyMain.class, args);
+    }
+}
